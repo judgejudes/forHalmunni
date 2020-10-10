79be56442bab5d0eb99827fdd0977a1fa2a3a927
judgejudes <judylee2021@u.northwestern.edu>
2020-10-09 22:56:20 -0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>안녕하세요, 할머니! 서정입니다</Text>
     </View>
   );
 }
@@ -18,4 +16,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#000',
+    fontSize: 16,
+  }
 });
