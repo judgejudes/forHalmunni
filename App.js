b7d1cb29c8f4ef29afacd144796068225617f49b
judgejudes <judylee2021@u.northwestern.edu>
2020-10-10 13:40:25 -0400

    can show the links

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const Banner = ({title}) => (
+  <Text style={styles.text}>{title}</Text>
+);
+
+const linksies = {
+  title: "안녕하세요, 할머니! 서정입니다",
+  youtubeLinks: [
+    {
+      "id": "Rain Sounds",
+      "title": "ult rain sounds yeye",
+      // "link"
+    },
+    {
+      "id": "Rain Sounds",
+      "title": "ult rain sounds yeye",
+      // "link"
+    }
+  ]
+};
+
+//app passes list of links to LinksList
+//LinksList makes a widget for each link
+const LinksList = ({ links }) => (
+  <View style={styles.linksList}>
+    { links.map(link => <Link link={link} />) }
+  </View>
+)
+
+const getLinkTitle = link => (
+  link.id
+);
+
+const Link = ({link}) => (
+  <TouchableOpacity style={styles.linkButton}>
+    <Text style={styles.linkText}>
+    {`hai ${getLinkTitle(link)}`}
+    </Text>
+  </TouchableOpacity>
+);
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>안녕하세요, 할머니! 서정입니다 ummmm</Text>
+      <Banner title={linksies.title}/>
+      <LinksList links={linksies.youtubeLinks}/>
     </View>
   );
 }
@@ -21,3 +62,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   }
 });
+
+export default App;
